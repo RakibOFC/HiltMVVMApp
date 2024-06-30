53325559f2f9924357de2840f392bf670e6632e3
Rakibul Islam <rakibull2000@gmail.com>
2024-06-30 11:56:34 +0600

    Impelemt HILT Constructor Injection Problem

--- app/src/main/java/com/rakibofc/hiltmvvmapp/MainActivity.kt
+++ app/src/main/java/com/rakibofc/hiltmvvmapp/MainActivity.kt
@@ -20,6 +20,9 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var exampleClass2: ExampleClass2
 
+    //@Inject
+    //lateinit var exampleClass4: ExampleClass4
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,7 +30,12 @@ class MainActivity : AppCompatActivity() {
 
         Log.e("TAG", "exampleClass1 - doAThing: ${exampleClass1.doAThing()}")
         Log.e("TAG", "exampleClass1 - sum: ${exampleClass1.sum()}")
-        Log.e("TAG", "exampleClass2 - exampleMethod2 with exampleMethod3: ${exampleClass2.exampleMethod2()}")
+        Log.e(
+            "TAG",
+            "exampleClass2 - exampleMethod2 with exampleMethod3: ${exampleClass2.exampleMethod2()}"
+        )
+
+        //Log.e("TAG", "exampleClass4 - exampleMethod4: ${exampleClass4.exampleMethod4()}")
     }
 }
 
@@ -75,4 +83,25 @@ class ExampleClass3 @Inject constructor() {
     fun exampleMethod3(): String {
         return "ExampleMethod3 in ExampleClass3"
     }
+}
+
+// This section will create compile time error
+class ExampleClass4 @Inject constructor(
+    private val exampleInterface1: ExampleInterface1
+) {
+    fun exampleMethod4(): String {
+        return "ExampleMethod4 in ExampleClass4 : ${exampleInterface1.interfaceMethod1()}"
+    }
+}
+
+class ExampleInterface2Impl
+@Inject
+constructor() : ExampleInterface1 {
+    override fun interfaceMethod1(): String {
+        return "ExampleInterface2Impl"
+    }
+}
+
+interface ExampleInterface1 {
+    fun interfaceMethod1(): String
 }
