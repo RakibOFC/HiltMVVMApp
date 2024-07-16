7ac3d7f88ab5e4abebf3e75a279f455ab5bb624a
Rakibul Islam <rakibull2000@gmail.com>
2024-07-16 23:57:52 +0600

    Remove unnecessary codes

--- app/src/main/java/com/rakibofc/hiltmvvmapp/data/remote/ApiService.kt
+++ app/src/main/java/com/rakibofc/hiltmvvmapp/data/remote/ApiService.kt
@@ -1,16 +1,10 @@
 package com.rakibofc.hiltmvvmapp.data.remote
 
 import com.rakibofc.hiltmvvmapp.domain.model.ContactInfoResponse
-import com.rakibofc.hiltmvvmapp.presentation.util.Resource
-import com.rakibofc.hiltmvvmapp.domain.model.User
 import retrofit2.http.GET
 
 interface ApiService {
 
     @GET("parents/contact-info/get/8801917272522")
     suspend fun getUsers(): ContactInfoResponse
-
-    interface IUserRepository {
-        suspend fun getUsers(): Resource<List<User>>
-    }
 }
